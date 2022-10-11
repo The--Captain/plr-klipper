74f88cb1603c3ab2aa064fe4ded294136337777a
Jake <jake.tebbett@gmail.com>
2022-10-11 09:26:57 +0100

    Various
    Added support for offset (untested as it tried to extrude 600mm of filament on test)
    Filenames with spaces
    Instructional comment to mark executable

--- plr.sh
+++ plr.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-SD_PATH=~octoprint/.octoprint/uploads
-cat ${2} > /tmp/plrtmpA.$$
-cat /tmp/plrtmpA.$$ | sed -e '1,/Z'${1}'/ d' | sed -ne '/ Z/,$ p' | grep -m 1 ' Z' | sed -ne 's/.* Z\([^ ]*\)/SET_KINEMATIC_POSITION Z=\1/p' > ${SD_PATH}/plr.gcode
+# you need to run this command on this file: sudo chmod +x plr.sh
+SD_PATH=~/gcode_files
+HEIGHT=${1}
+ADJHEIGHT=${2}
+shift 2
+I=${@}
+cat "$I" > /tmp/plrtmpA.$$
+cat /tmp/plrtmpA.$$ | sed -e '1,/Z'${ADJHEIGHT}'/ d' | sed -ne '/ Z/,$ p' | grep -m 1 ' Z' | sed -ne 's/.* Z\([^ ]*\)/SET_KINEMATIC_POSITION Z=\1/p' > ${SD_PATH}/plr.gcode
 echo 'G91' >> ${SD_PATH}/plr.gcode
 echo 'G1 Z5' >> ${SD_PATH}/plr.gcode
 echo 'G90' >> ${SD_PATH}/plr.gcode
-echo 'G28 X Y' >> ${SD_PATH}/plr.gcode
-echo 'START_TEMPS' >> ${SD_PATH}/plr.gcode
-cat /tmp/plrtmpA.$$ | sed '/ Z'${1}'/q' | sed -ne '/\(M104\|M140\|M109\|M190\|M106\)/p' >> ${SD_PATH}/plr.gcode
+echo 'G28 X0 Y0' >> ${SD_PATH}/plr.gcode
+echo '_START_TEMPS' >> ${SD_PATH}/plr.gcode
+cat /tmp/plrtmpA.$$ | sed '/ Z'${HEIGHT}'/q' | sed -ne '/\(M104\|M140\|M109\|M190\|M106\)/p' >> ${SD_PATH}/plr.gcode
 cat /tmp/plrtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_bed_temperature | sed -ne 's/.* = /M140 S/p' | head -1 >> ${SD_PATH}/plr.gcode
 cat /tmp/plrtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_print_temperature | sed -ne 's/.* = /M104 S/p' | head -1 >> ${SD_PATH}/plr.gcode
 cat /tmp/plrtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_bed_temperature | sed -ne 's/.* = /M190 S/p' | head -1 >> ${SD_PATH}/plr.gcode
 cat /tmp/plrtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_print_temperature | sed -ne 's/.* = /M109 S/p' | head -1 >> ${SD_PATH}/plr.gcode
-cat /tmp/plrtmpA.$$ | sed -e '1,/Z'${1}'/ d' | sed -e '/ Z/q' | tac | grep -m 1 ' E' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p' >> ${SD_PATH}/plr.gcode
+cat /tmp/plrtmpA.$$ | sed -e '1,/Z'${HEIGHT}'/ d' | sed -e '/ Z/q' | tac | grep -m 1 ' E' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p' >> ${SD_PATH}/plr.gcode
 echo 'G91' >> ${SD_PATH}/plr.gcode
 echo 'G1 Z-5' >> ${SD_PATH}/plr.gcode
 echo 'G90' >> ${SD_PATH}/plr.gcode
-cat /tmp/plrtmpA.$$ | sed -e '1,/Z'${1}'/ d' | sed -ne '/ Z/,$ p' >> ${SD_PATH}/plr.gcode
+cat /tmp/plrtmpA.$$ | sed -e '1,/Z'${HEIGHT}'/ d' | sed -ne '/ Z/,$ p' >> ${SD_PATH}/plr.gcode
