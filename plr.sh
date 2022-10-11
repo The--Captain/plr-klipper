8c9356cc644fb501c88f880ce3a5cee2f97dd8ad
pappicio <52137123+pappicio@users.noreply.github.com>
2022-10-11 19:26:09 +0200

    Add files via upload

--- plr.sh
+++ plr.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-SD_PATH=~octoprint/.octoprint/uploads
-cat ${2} > /tmp/plrtmpA.$$
+###SD_PATH=~octoprint/.octoprint/uploads
+
+############################################
+SD_PATH=/home/pi/gcode_files
+
+############################################
+
+cat ${SD_PATH}/${2} > /tmp/plrtmpA.$$
 cat /tmp/plrtmpA.$$ | sed -e '1,/Z'${1}'/ d' | sed -ne '/ Z/,$ p' | grep -m 1 ' Z' | sed -ne 's/.* Z\([^ ]*\)/SET_KINEMATIC_POSITION Z=\1/p' > ${SD_PATH}/plr.gcode
 echo 'G91' >> ${SD_PATH}/plr.gcode
 echo 'G1 Z5' >> ${SD_PATH}/plr.gcode
 echo 'G90' >> ${SD_PATH}/plr.gcode
 echo 'G28 X Y' >> ${SD_PATH}/plr.gcode
-echo 'START_TEMPS' >> ${SD_PATH}/plr.gcode
+echo 'M118 START_TEMPS...' >> ${SD_PATH}/plr.gcode
 cat /tmp/plrtmpA.$$ | sed '/ Z'${1}'/q' | sed -ne '/\(M104\|M140\|M109\|M190\|M106\)/p' >> ${SD_PATH}/plr.gcode
 cat /tmp/plrtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_bed_temperature | sed -ne 's/.* = /M140 S/p' | head -1 >> ${SD_PATH}/plr.gcode
 cat /tmp/plrtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_print_temperature | sed -ne 's/.* = /M104 S/p' | head -1 >> ${SD_PATH}/plr.gcode
